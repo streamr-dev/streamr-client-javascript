e029a890e71a47db9f57e61ab43a9db616220770
Aapeli <aapeli.haanpuu@streamr.com>
2018-06-01 16:41:24 +0200

    Add mocha to eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,8 @@
 module.exports = exports = {
     extends: 'streamr',
+    env: {
+        mocha: true,
+    },
     rules: {
         'no-plusplus': ["error", { "allowForLoopAfterthoughts": true }],
         'no-underscore-dangle': ["error", { "allowAfterThis": true }]
