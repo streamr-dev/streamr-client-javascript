ee782bb05259683c16544ecbfed65cdafc4d2a46
Aapeli <aapeli.haanpuu@streamr.com>
2018-09-26 16:49:53 +0300

    Changed shim files to use es5

--- src/shim/http-https.js
+++ src/shim/http-https.js
@@ -1,7 +1,5 @@
+// NB: THIS FILE MUST BE IN ES5
+
 // Browser native fetch implementation does not the http(s).Agent like node-fetch does
 
-class Agent {}
-
-module.exports = {
-    Agent,
-}
+exports.Agent = function Agent() {}
