075dcc07294800387c95807ae0d16b61d7a14677
Aapeli Haanpuu <aapeli.haanpuu@streamr.com>
2018-09-26 20:05:24 +0300

    Fix typo

--- src/shim/http-https.js
+++ src/shim/http-https.js
@@ -1,5 +1,5 @@
 // NB: THIS FILE MUST BE IN ES5
 
-// Browser native fetch implementation does not the http(s).Agent like node-fetch does
+// Browser native fetch implementation does not support the http(s) Agent like node-fetch does
 
 exports.Agent = function Agent() {}
