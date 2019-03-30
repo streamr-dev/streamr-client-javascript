4e3e4b3c28d510a8700a7d0c43f51c5fd72cfda4
Tim Oxley <secoif@gmail.com>
2019-03-30 12:59:00 +0800

    Add subscription subscribe/unsubscribe event order tests.

---
+++ test/integration/Subscription.test.js
@@ -0,0 +1,102 @@
+import { ethers } from 'ethers'
+import uniqueId from 'lodash/uniqueId'
+import StreamrClient from '../../src'
+import config from './config'
+
+const createClient = (opts = {}) => new StreamrClient({
+    url: config.websocketUrl,
+    restUrl: config.restUrl,
+    auth: {
+        privateKey: ethers.Wallet.createRandom().privateKey,
+    },
+    autoConnect: false,
+    autoDisconnect: false,
+    ...opts,
+})
+
+const throwError = (error) => { throw error }
+
+const RESEND_ALL = {
+    from: {
+        timestamp: 0,
+    },
+}
+
+describe('Subscription', () => {
+    let stream
+    let client
+    let subscription
+
+    async function setup() {
+        client = createClient()
+        client.on('error', throwError)
+        stream = await client.createStream({
+            name: uniqueId(),
+        })
+    }
+
+    async function teardown() {
+        if (stream) {
+            await stream.delete()
+            stream = undefined
+        }
+        if (subscription) {
+            await client.unsubscribe(subscription)
+            subscription = undefined
+        }
+        if (client && client.isConnected()) {
+            await client.disconnect()
+            client.off('error', throwError)
+            client = undefined
+        }
+    }
+
+    /**
+     * Returns an array which will be filled with subscription events in the order they occur.
+     * Needs to create subscription at same time in order to track message events.
+     */
+
+    function createMonitoredSubscription(opts = {}) {
+        const events = []
+        subscription = client.subscribe({
+            stream: stream.id,
+            resend: RESEND_ALL,
+            ...opts,
+        }, (message) => {
+            events.push(message)
+        })
+        subscription.on('subscribed', () => events.push('subscribed'))
+        subscription.on('resending', () => events.push('resending'))
+        subscription.on('resent', () => events.push('resent'))
+        subscription.on('unsubscribed', () => events.push('unsubscribed'))
+        subscription.on('error', () => events.push('unsubscribed'))
+        return events
+    }
+
+    beforeEach(async () => {
+        await teardown()
+        await setup()
+    })
+
+    afterEach(async () => {
+        await teardown()
+    })
+
+    describe('subscribe/unsubscribe events', () => {
+        it('fires events in correct order', async (done) => {
+            const subscriptionEvents = createMonitoredSubscription()
+            subscription.on('subscribed', async () => {
+                subscription.on('unsubscribed', () => {
+                    expect(subscriptionEvents).toEqual([
+                        'subscribed',
+                        'unsubscribed',
+                    ])
+                    done()
+                })
+                await client.unsubscribe(subscription)
+            })
+
+            await client.connect()
+        })
+    })
+})
