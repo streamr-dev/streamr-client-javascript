0a617866a7af0046971ca54ebf92e1e3bca6a3ad
Kare Nuorteva <kare.nuorteva@me.com>
2019-09-23 12:19:19 +0300

    Refactor
    - Add createSecret() for testing

--- src/rest/CommunityEndpoints.js
+++ src/rest/CommunityEndpoints.js
@@ -6,8 +6,9 @@ import authFetch from './authFetch'
 const CommunityProduct = require('../../CommunityProduct.json')
 
 export async function joinCommunity(communityAddress, memberAddress, secret = undefined) {
-    const json = authFetch(
-        `${this.options.restUrl}/communities/${communityAddress}/joinRequests`,
+    const url = `${this.options.restUrl}/communities/${communityAddress}/joinRequests`
+    return authFetch(
+        url,
         this.session,
         {
             method: 'POST',
@@ -20,28 +21,43 @@ export async function joinCommunity(communityAddress, memberAddress, secret = un
             },
         },
     )
-    return json
 }
 
 export async function memberStats(communityAddress, memberAddress) {
-    const json = await fetch(
-        `${this.options.restUrl}/communities/${communityAddress}/members/${memberAddress}`,
-        {}
-    ).then((res) => res.json())
-    return json
+    const url = `${this.options.restUrl}/communities/${communityAddress}/members/${memberAddress}`
+    return fetch(url).then((res) => res.json())
 }
 
 export async function withdraw(communityAddress, memberAddress, wallet, confirmations = 1) {
-    const stats = memberStats(communityAddress, memberAddress)
+    const stats = await this.memberStats(communityAddress, memberAddress)
+    if (!stats.withdrawableBlockNumber) {
+        throw new Error('No earnings to withdraw.')
+    }
     const contract = new Contract(communityAddress, CommunityProduct.abi, wallet)
     const withdrawTx = await contract.withdrawAll(stats.withdrawableBlockNumber, stats.withdrawableEarnings, stats.proof)
     await withdrawTx.wait(confirmations)
 }
 
 export async function communityStats(communityAddress) {
-    const json = fetch(
-        `${this.options.restUrl}/communities/${communityAddress}/stats`,
-        {},
-    ).then((res) => res.json())
-    return json
+    const url = `${this.options.restUrl}/communities/${communityAddress}/stats`
+    return fetch(url).then((res) => res.json())
 }
+
+export async function createSecret(communityAddress, secret, name = 'Untitled Community Secret') {
+    const url = `${this.options.restUrl}/communities/${communityAddress}/secrets`
+    return authFetch(
+        url,
+        this.session,
+        {
+            method: 'POST',
+            body: JSON.stringify({
+                name,
+                secret,
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        },
+    )
+}
+
