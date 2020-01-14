a95185c1498c0fd69a2fe6236c2c6bd0595a8bef
Juuso Takalainen <juuso.takalainen@streamr.com>
2020-01-14 12:19:14 +0200

    fix eslint warnings

--- test/unit/StubbedStreamrClient.js
+++ test/unit/StubbedStreamrClient.js
@@ -3,7 +3,6 @@ import StreamrClient from '../../src/StreamrClient'
 import Stream from '../../src/rest/domain/Stream'
 
 export default class StubbedStreamrClient extends StreamrClient {
-    /* eslint-disable class-methods-use-this */
     getUserInfo() {
         return Promise.resolve({
             username: 'username',
@@ -14,5 +13,4 @@ export default class StubbedStreamrClient extends StreamrClient {
         id: 'streamId',
         partitions: 1,
     }))
-    /* eslint-enable class-methods-use-this */
 }
