73d825f3511cd2c7789b1c7f6f8abe358e1b9493
mthambipillai <melchior.thambipillai@streamr.com>
2020-03-27 13:57:24 +0100

    fix unit test

--- test/unit/StubbedStreamrClient.js
+++ test/unit/StubbedStreamrClient.js
@@ -13,6 +13,8 @@ export default class StubbedStreamrClient extends StreamrClient {
         id: 'streamId',
         partitions: 1,
     }))
+
+    getStreamSubscribers = sinon.stub().resolves([])
 }
 // publisherId is the hash of 'username'
 StubbedStreamrClient.hashedUsername = '0x16F78A7D6317F102BBD95FC9A4F3FF2E3249287690B8BDAD6B7810F82B34ACE3'.toLowerCase()
