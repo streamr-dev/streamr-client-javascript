6299ff77f922728cea6b834d36775609b8a5439c
Tim Oxley <secoif@gmail.com>
2020-04-01 11:48:46 -0400

    Correctly reuse default headers after auth failure authFetch.

--- src/rest/authFetch.js
+++ src/rest/authFetch.js
@@ -11,6 +11,13 @@ export const DEFAULT_HEADERS = {
 const debug = debugFactory('StreamrClient:utils')
 
 const authFetch = async (url, session, opts = {}, requireNewToken = false) => {
+    const options = {
+        headers: {
+            ...DEFAULT_HEADERS,
+            ...opts.headers,
+        }
+    }
+
     debug('authFetch: ', url, opts)
 
     const response = await fetch(url, {
@@ -19,8 +26,7 @@ const authFetch = async (url, session, opts = {}, requireNewToken = false) => {
             ...(session && !session.options.unauthenticated ? {
                 Authorization: `Bearer ${await session.getSessionToken(requireNewToken)}`,
             } : {}),
-            ...DEFAULT_HEADERS,
-            ...opts.headers,
+            ...options.headers,
         },
     })
 
@@ -33,7 +39,7 @@ const authFetch = async (url, session, opts = {}, requireNewToken = false) => {
             throw new AuthFetchError(e.message, response, body)
         }
     } else if ([400, 401].includes(response.status) && !requireNewToken) {
-        return authFetch(url, session, opts, true)
+        return authFetch(url, session, options, true)
     } else {
         throw new AuthFetchError(`Request to ${url} returned with error code ${response.status}.`, response, body)
     }
