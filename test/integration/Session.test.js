b61c2c7d6441d7ecc21d8f8bee891ee0d08b4d8c
Tim Oxley <secoif@gmail.com>
2020-05-09 15:28:23 -0400

    Linting.

--- test/integration/Session.test.js
+++ test/integration/Session.test.js
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers'
-import { wait, waitForCondition } from 'streamr-test-utils'
+import { wait } from 'streamr-test-utils'
 
 import { uid } from '../utils'
 import StreamrClient from '../../src'
