1a9a82c1da974df7bc6af3b1dccdfda08001d490
Tim Oxley <secoif@gmail.com>
2020-05-25 13:26:56 -0400

    Release 4.0.0-beta.2

--- test/integration/DataUnionEndpoints.test.js
+++ test/integration/DataUnionEndpoints.test.js
@@ -12,9 +12,6 @@ const log = debug('StreamrClient::DataUnionEndpoints::integration-test')
 function Test(client, id, log) {
   return new Promise(async (resolve) => {
     client = client || new StreamrClient({
-        //auth: {
-            //privateKey: config.privateKey,
-        //},
         autoConnect: false,
         autoDisconnect: false,
         ...config.clientOptions,
@@ -38,14 +35,14 @@ function Test(client, id, log) {
         log('subscribed to ', id)
         resolve()
     })
-        //.on('error', (error) => log('error', error))
-        //.on('no_resend', () => log('no_resend'))
-        //.on('resent', () => log('resent'))
-        //.on('groupKeyMissing', (...args) => log('groupKeyMissing', ...args))
-        //.on('unsubscribed', () => log('unsubscribed'))
-        //.on('gap', (...args) => log('gap', ...args))
-        //.on('disconnected', () => log('disconnected'))
-        //.on('done', () => log('done'))
+        .on('error', (error) => log('error', error))
+        .on('no_resend', () => log('no_resend'))
+        .on('resent', () => log('resent'))
+        .on('groupKeyMissing', (...args) => log('groupKeyMissing', ...args))
+        .on('unsubscribed', () => log('unsubscribed'))
+        .on('gap', (...args) => log('gap', ...args))
+        .on('disconnected', () => log('disconnected'))
+        .on('done', () => log('done'))
     })
 }
 
@@ -109,13 +106,13 @@ describe('DataUnionEndPoints', () => {
             await adminClient.dataUnionIsReady(dataUnion.address, log)
 
             const joinPartStreamId = await adminClient.getJoinPartStreamId(dataUnion.address, testProvider)
+
+            await adminClient.addMembers(dataUnion.address, memberAddressList, testProvider)
+            //await wait(5000)
             await Promise.all([
                 Test(adminClient, joinPartStreamId, debug('Streamr::adminClient')),
                 //Test(undefined, joinPartStreamId, debug('Streamr::otherClient')),
             ])
-
-            //await wait(5000)
-            await adminClient.addMembers(dataUnion.address, memberAddressList, testProvider)
             await adminClient.hasJoined(dataUnion.address, memberAddressList[0])
             const res = await adminClient.getDataUnionStats(dataUnion.address)
             expect(res.memberCount).toEqual({
