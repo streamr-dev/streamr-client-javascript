d32b2edc1993f2bab2fd9179581bad91cb296ea7
Tim Oxley <secoif@gmail.com>
2020-09-18 12:15:45 -0400

    Install streamr-client-protocol@5.1.0

--- test/unit/MessageCreationUtil.test.js
+++ test/unit/MessageCreationUtil.test.js
@@ -156,7 +156,7 @@ describe('MessageCreationUtil', () => {
                 messageId: new MessageID(streamId, 0, timestamp, sequenceNumber, hashedUsername, msgCreationUtil.msgChainId),
                 prevMesssageRef: prevMsgRef,
                 content: pubMsg,
-                contentType: StreamMessage.CONTENT_TYPES.MESSAGE,
+                messageType: StreamMessage.MESSAGE_TYPES.MESSAGE,
                 encryptionType: StreamMessage.ENCRYPTION_TYPES.NONE,
                 signatureType: StreamMessage.SIGNATURE_TYPES.ETH,
                 signature: 'signature',
