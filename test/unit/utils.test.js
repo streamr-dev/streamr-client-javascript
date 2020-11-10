dec789de850f43a906ce92ea1ae6af331845c33d
Tim Oxley <secoif@gmail.com>
2020-11-10 11:09:24 -0500

    Add pUpDownSteps util.

--- test/unit/utils.test.js
+++ test/unit/utils.test.js
@@ -1,11 +1,14 @@
+import Emitter from 'events'
+
 import sinon from 'sinon'
-import debugFactory from 'debug'
+import Debug from 'debug'
+import { wait } from 'streamr-test-utils'
+import express from 'express'
 
 import authFetch from '../../src/rest/authFetch'
-import { uuid } from '../../src/utils'
+import { uuid, pUpDownSteps, Defer } from '../../src/utils'
 
-const debug = debugFactory('StreamrClient::test::utils')
-const express = require('express')
+const debug = Debug('StreamrClient::test::utils')
 
 describe('utils', () => {
     let session
@@ -84,6 +87,235 @@ describe('utils', () => {
         it('increments', () => {
             const uid = uuid('test') // generate new text to ensure count starts at 1
             expect(uuid(uid) < uuid(uid)).toBeTruthy()
+        })
+    })
+
+    describe('pUpDownSteps', () => {
+        let order
+        let up
+        let down
+        let emitter
+
+        beforeEach(() => {
+            if (emitter) {
+                emitter.removeAllListeners()
+            }
+
+            order = []
+            const currentOrder = order
+            emitter = new Emitter()
+
+            emitter.on('next', (name, v = '') => {
+                currentOrder.push(`${name} ${v}`.trim())
+            })
+
+            const currentEmitter = emitter
+            up = async (...args) => {
+                currentEmitter.emit('next', 'up start', ...args)
+                await wait(50)
+                currentEmitter.emit('next', 'up end', ...args)
+            }
+
+            down = async (...args) => {
+                currentEmitter.emit('next', 'down start', ...args)
+                await wait(10)
+                currentEmitter.emit('next', 'down end', ...args)
+            }
+        })
+
+        it('calls up/down only once', async () => {
+            let shouldUp = true
+            const next = pUpDownSteps([
+                async () => {
+                    await up()
+                    return () => down()
+                }
+            ], () => shouldUp)
+
+            await Promise.all([
+                next(),
+                next()
+            ])
+
+            shouldUp = false
+
+            await Promise.all([
+                next(),
+                next()
+            ])
+
+            expect(order).toEqual([
+                'up start',
+                'up end',
+                'down start',
+                'down end',
+            ])
+        })
+
+        it('calls down automatically if check is false after up complete', async () => {
+            let shouldUp = true
+            const next = pUpDownSteps([
+                async () => {
+                    await up()
+                    shouldUp = false
+                    return () => down()
+                }
+            ], () => shouldUp)
+
+            await next()
+
+            expect(order).toEqual([
+                'up start',
+                'up end',
+                'down start',
+                'down end',
+            ])
+        })
+
+        it('does nothing if check fails', async () => {
+            const shouldUp = false
+            const next = pUpDownSteps([
+                async () => {
+                    await up()
+                    return () => down()
+                }
+            ], () => shouldUp)
+
+            await next()
+
+            expect(order).toEqual([])
+        })
+
+        it('only calls one at a time when down called during up', async () => {
+            let shouldUp = true
+
+            const next = pUpDownSteps([
+                async () => {
+                    await up()
+                    return () => down()
+                }
+            ], () => shouldUp)
+            const done = Defer()
+            emitter.on('next', async (name) => {
+                if (name === 'up start') {
+                    shouldUp = false
+                    await next()
+                    expect(order).toEqual([
+                        'up start',
+                        'up end',
+                        'down start',
+                        'down end',
+                    ])
+                    done.resolve()
+                }
+            })
+
+            await next()
+
+            expect(order).toEqual([
+                'up start',
+                'up end',
+                'down start',
+                'down end',
+            ])
+
+            await done
+        })
+
+        describe('plays undo stack at point of state change', () => {
+            let shouldUp
+            let next
+            beforeEach(() => {
+                shouldUp = false
+
+                next = pUpDownSteps([
+                    async () => {
+                        await up('a')
+                        return () => down('a')
+                    },
+                    async () => {
+                        await up('b')
+                        return () => down('b')
+                    },
+                    async () => {
+                        await up('c')
+                        return () => down('c')
+                    },
+                ], () => shouldUp)
+            })
+
+            it('plays all up steps in order, then down steps in order', async () => {
+                shouldUp = true
+                await next()
+                expect(order).toEqual([
+                    'up start a',
+                    'up end a',
+                    'up start b',
+                    'up end b',
+                    'up start c',
+                    'up end c',
+                ])
+                shouldUp = false
+                await next()
+                expect(order).toEqual([
+                    'up start a',
+                    'up end a',
+                    'up start b',
+                    'up end b',
+                    'up start c',
+                    'up end c',
+                    'down start c',
+                    'down end c',
+                    'down start b',
+                    'down end b',
+                    'down start a',
+                    'down end a',
+                ])
+            })
+
+            it('can stop before first step', async () => {
+                shouldUp = true
+                const done = Defer()
+                emitter.on('next', async (name, v) => {
+                    if (name === 'up start' && v === 'a') {
+                        shouldUp = false
+                        done.resolve()
+                    }
+                })
+
+                await next()
+                expect(order).toEqual([
+                    'up start a',
+                    'up end a',
+                    'down start a',
+                    'down end a',
+                ])
+                await done
+            })
+
+            it('can stop before second step', async () => {
+                shouldUp = true
+                const done = Defer()
+                emitter.on('next', async (name, v) => {
+                    if (name === 'up end' && v === 'b') {
+                        shouldUp = false
+                        done.resolve()
+                    }
+                })
+
+                await next()
+                expect(order).toEqual([
+                    'up start a',
+                    'up end a',
+                    'up start b',
+                    'up end b',
+                    'down start b',
+                    'down end b',
+                    'down start a',
+                    'down end a',
+                ])
+                await done
+            })
         })
     })
 })
