e1a85300565c72b5e4cebb36a158c1ad14679168
Tim Oxley <secoif@gmail.com>
2020-12-19 16:55:30 -0500

    Enable bundle analysis in CI build.

--- webpack.config.js
+++ webpack.config.js
@@ -122,6 +122,7 @@ module.exports = (env, argv) => {
                 new BundleAnalyzerPlugin({
                     analyzerMode: 'static',
                     openAnalyzer: false,
+                    generateStatsFile: true,
                 }),
             ] : [])
         ]
