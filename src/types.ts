d4d9c6c7f4fca3784e66fa83887fa6a0a1929e2e
Teo Gebhard <teo@streamr.com>
2021-03-08 13:16:15 +0200

    Export Todo type

--- src/types.ts
+++ src/types.ts
@@ -4,5 +4,4 @@ export type EthereumAddress = string
 
 export type MaybeAsync<T extends F.Function> = T | F.Promisify<T> // Utility Type: make a function maybe async
 
-/** @internal */
 export type Todo = any
