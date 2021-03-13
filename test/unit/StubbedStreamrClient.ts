30a05da46e2cd3d85618e64acfa218a6c8def376
Teo Gebhard <teo@streamr.com>
2021-03-13 12:41:32 +0200

    Unit tests

--- test/unit/StubbedStreamrClient.ts
+++ test/unit/StubbedStreamrClient.ts
@@ -2,6 +2,7 @@ import { StreamrClient } from '../../src/'
 import { Stream } from '../../src/stream'
 
 export default class StubbedStreamrClient extends StreamrClient {
+    // @ts-expect-error
     getUserInfo() {
         return Promise.resolve({
             username: 'username',
@@ -9,11 +10,12 @@ export default class StubbedStreamrClient extends StreamrClient {
     }
 
     async getStream () {
-        return new Stream(null, {
+        return new Stream(null as any, {
             id: 'streamId',
             partitions: 1,
         })
     }
 }
 // publisherId is the hash of 'username'
+// @ts-expect-error
 StubbedStreamrClient.hashedUsername = '0x16F78A7D6317F102BBD95FC9A4F3FF2E3249287690B8BDAD6B7810F82B34ACE3'.toLowerCase()
