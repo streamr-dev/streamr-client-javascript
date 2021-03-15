56f55a3a8b7c8247d46115ce79ea90a6f789bff1
Teo Gebhard <teo@streamr.com>
2021-03-15 19:24:22 +0200

    Rename client factory

--- test/unit/Config.test.ts
+++ test/unit/Config.test.ts
@@ -31,7 +31,7 @@ describe('Config', () => {
     })
 
     describe('private key', () => {
-        const createClient = (privateKey: BytesLike) => {
+        const createAuthenticatedClient = (privateKey: BytesLike) => {
             return new StreamrClient({
                 auth: {
                     privateKey
@@ -39,11 +39,11 @@ describe('Config', () => {
             })
         }
         it('string', async () => {
-            const client = createClient('0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF')
+            const client = createAuthenticatedClient('0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF')
             expect(await client.getAddress()).toBe('0xFCAd0B19bB29D4674531d6f115237E16AfCE377c')
         })
         it('byteslike', async () => {
-            const client = createClient(arrayify('0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF'))
+            const client = createAuthenticatedClient(arrayify('0x0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF0123456789ABCDEF'))
             expect(await client.getAddress()).toBe('0xFCAd0B19bB29D4674531d6f115237E16AfCE377c')
         })
     })
